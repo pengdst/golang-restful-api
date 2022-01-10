ae11a9e6da90abdf147218ec248d441924ac5344
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-01-10 23:01:12 +0700

    Listen and Serve router

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"pengdst/golang-restful-api/app"
+	"pengdst/golang-restful-api/controller"
+	"pengdst/golang-restful-api/helper"
+	"pengdst/golang-restful-api/repository"
+	"pengdst/golang-restful-api/service"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+
+	categortRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categortRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/categories", categoryController.GetAll)
+	router.POST("/categories", categoryController.Create)
+	router.GET("/categories/:categoryId", categoryController.FindById)
+	router.PUT("/categories/:categoryId", categoryController.Update)
+	router.DELETE("/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
