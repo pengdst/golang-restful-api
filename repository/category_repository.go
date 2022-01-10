2851c9780030fb7c3b1fb5d2c74e0e1dd311b715
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-01-10 20:20:40 +0700

    add return error

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -10,6 +10,6 @@ type CategoryRepository interface {
 	Create(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
 	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
 	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
-	FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
 	GetAll(ctx context.Context, tx *sql.Tx) []domain.Category
 }
