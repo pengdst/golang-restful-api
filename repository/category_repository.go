f2d96ef4ca5ed449fbdcd38e2122f7da60c590b0
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-01-10 20:02:02 +0700

    fix sql params to pointer

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -7,9 +7,9 @@ import (
 )
 
 type CategoryRepository interface {
-	Create(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
-	Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
-	Delete(ctx context.Context, tx sql.Tx, category domain.Category)
-	FindById(ctx context.Context, tx sql.Tx, categoryId int) domain.Category
-	GetAll(ctx context.Context, tx sql.Tx) []domain.Category
+	Create(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category
+	GetAll(ctx context.Context, tx *sql.Tx) []domain.Category
 }
