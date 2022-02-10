74fe2a0e599c097719a81b4d018d5f821bf300c1
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-02-10 12:37:42 +0700

    add column created at and updated at, gorm support

--- model/domain/category.go
+++ model/domain/category.go
@@ -1,6 +1,14 @@
 package domain
 
+import "time"
+
 type Category struct {
-	Id   int
-	Name string
+	Id        int `gorm:"primaryKey"`
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (Category) TableName() string {
+	return "category"
 }
