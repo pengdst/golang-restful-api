de31df70ae72e95c5e79b5027884bd7b9e2b4911
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-02-10 12:36:49 +0700

    create function NewGormDB

--- app/database.go
+++ app/database.go
@@ -4,7 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"pengdst/golang-restful-api/helper"
+	"pengdst/golang-restful-api/model/domain"
 	"time"
 )
 
@@ -27,4 +30,31 @@ func NewDB() *sql.DB {
 	db.SetConnMaxIdleTime(10 * time.Minute)
 
 	return db
+}
+
+func NewGormDB() *gorm.DB {
+	var (
+		user       = "root"
+		password   = "root"
+		dbHost     = "localhost"
+		dbPort     = "3306"
+		dbDatabase = "golang-restful-api"
+	)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, dbHost, dbPort, dbDatabase)
+	gormDb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	helper.PanicIfError(err)
+
+	err = gormDb.AutoMigrate(&domain.Category{})
+	helper.PanicIfError(err)
+
+	db, err := gormDb.DB()
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return gormDb
 }
