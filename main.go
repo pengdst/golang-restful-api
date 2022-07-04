205b64208203385221ce243cfffd0c3f5ec3dea8
pengdst <pengkuh.dst@gmail.com>
2022-07-04 12:21:08 +0700

    run server with gin

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
-	"net/http"
+	"os"
 	"pengdst/golang-restful-api/app"
 	"pengdst/golang-restful-api/controller"
 	"pengdst/golang-restful-api/helper"
-	"pengdst/golang-restful-api/middleware"
 	"pengdst/golang-restful-api/repository"
 	"pengdst/golang-restful-api/service"
 )
@@ -28,11 +27,6 @@ func main() {
 
 	router := app.NewRouter(categoryController)
 
-	server := http.Server{
-		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
-	}
-
-	err := server.ListenAndServe()
+	err := router.Run(":" + os.Getenv("PORT"))
 	helper.PanicIfError(err)
 }
