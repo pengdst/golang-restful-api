496a1fd1a82273f3bcc4c72c157ea0571acbb41d
pengdst <pengkuh.dst@gmail.com>
2022-07-04 11:59:15 +0700

    change router with gin

--- app/router.go
+++ app/router.go
@@ -1,23 +1,22 @@
 package app
 
 import (
-	"github.com/julienschmidt/httprouter"
-	"net/http"
+	"github.com/gin-gonic/gin"
 	"pengdst/golang-restful-api/controller"
 	"pengdst/golang-restful-api/exception"
 )
 
-func NewRouter(categoryController controller.CategoryController) *httprouter.Router {
-	router := httprouter.New()
+func NewRouter(categoryController controller.CategoryController) *gin.Engine {
+	router := gin.Default()
 
-	router.ServeFiles("/docs/*filepath", http.Dir("./docs"))
+	router.Use(gin.CustomRecovery(exception.ErrorHandler))
+
+	router.Static("/docs", "./docs")
 	router.GET("/categories", categoryController.GetAll)
 	router.POST("/categories", categoryController.Create)
 	router.GET("/categories/:categoryId", categoryController.FindById)
 	router.PUT("/categories/:categoryId", categoryController.Update)
 	router.DELETE("/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
 
 	return router
 }
