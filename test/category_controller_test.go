db2ee659b0eee0c1545db14fffa078b5aa6debd6
pengdst <pengkuh.dst@gmail.com>
2022-07-04 13:05:50 +0700

    fix testing with gorm and gin

--- test/category_controller_test.go
+++ test/category_controller_test.go
@@ -1,19 +1,22 @@
 package test
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/assert/v2"
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"pengdst/golang-restful-api/app"
 	"pengdst/golang-restful-api/controller"
 	"pengdst/golang-restful-api/helper"
-	"pengdst/golang-restful-api/middleware"
 	"pengdst/golang-restful-api/repository"
 	"pengdst/golang-restful-api/service"
 	"strings"
@@ -21,8 +24,26 @@ import (
 	"time"
 )
 
-func setupTestDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/golang-restful-api-test")
+func init() {
+	err := godotenv.Load("../.env")
+	helper.PanicIfError(err)
+}
+
+func setupTestDB() *gorm.DB {
+	var (
+		user       = os.Getenv("DB_TEST_USERNAME")
+		password   = os.Getenv("DB_TEST_PASSWORD")
+		dbHost     = os.Getenv("DB_TEST_HOST")
+		dbPort     = os.Getenv("DB_TEST_PORT")
+		dbDatabase = os.Getenv("DB_TEST_NAME")
+	)
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		dbHost, dbPort, user, password, dbDatabase, "")
+	gormDb, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	helper.PanicIfError(err)
+
+	db, err := gormDb.DB()
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -30,23 +51,23 @@ func setupTestDB() *sql.DB {
 	db.SetConnMaxLifetime(60 * time.Minute)
 	db.SetConnMaxIdleTime(10 * time.Minute)
 
-	return db
+	return gormDb
 }
 
-func setupRouter(db *sql.DB) http.Handler {
+func setupRouter(db *gorm.DB) *gin.Engine {
 	validate := validator.New()
 
-	categortRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categortRepository, db, validate)
+	categortRepository := repository.NewGormCategoryRepository(db)
+	categoryService := service.NewGormCategoryService(categortRepository, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
 	router := app.NewRouter(categoryController)
 
-	return middleware.NewAuthMiddleware(router)
+	return router
 }
 
-func truncateDb(db *sql.DB) {
-	db.Exec("TRUNCATE category")
+func truncateDb(db *gorm.DB) {
+	db.Exec("TRUNCATE category restart identity")
 }
 
 func TestCreateCategorySuccess(t *testing.T) {
@@ -55,7 +76,7 @@ func TestCreateCategorySuccess(t *testing.T) {
 	router := setupRouter(db)
 
 	requestBody := strings.NewReader(`{"name":"New Gadget"}`)
-	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/categories", requestBody)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/categories", requestBody)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -64,14 +85,14 @@ func TestCreateCategorySuccess(t *testing.T) {
 	router.ServeHTTP(recorder, request)
 
 	response := recorder.Result()
-	assert.Equal(t, http.StatusOK, response.StatusCode)
+	assert.Equal(t, http.StatusCreated, response.StatusCode)
 
 	body, _ := io.ReadAll(response.Body)
 	fmt.Println(body)
 	var responseBody map[string]interface{}
 	_ = json.Unmarshal(body, &responseBody)
 
-	assert.Equal(t, http.StatusOK, int(responseBody["code"].(float64)))
+	assert.Equal(t, http.StatusCreated, int(responseBody["code"].(float64)))
 	assert.Equal(t, "Ok", responseBody["status"])
 	assert.NotEqual(t, nil, responseBody["data"])
 }
@@ -81,7 +102,7 @@ func TestCreateCategoryFailed(t *testing.T) {
 	router := setupRouter(db)
 
 	requestBody := strings.NewReader(`{"name":""}`)
-	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/categories", requestBody)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/categories", requestBody)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -106,7 +127,7 @@ func TestGetAllSuccess(t *testing.T) {
 	router := setupRouter(db)
 	TestCreateCategorySuccess(t)
 
-	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/categories", nil)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories", nil)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -136,7 +157,7 @@ func TestFindByIdSuccess(t *testing.T) {
 	TestCreateCategorySuccess(t)
 
 	requestBody := strings.NewReader(``)
-	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/categories/1", requestBody)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories/1", requestBody)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -162,7 +183,7 @@ func TestFindByIdFailed(t *testing.T) {
 	TestCreateCategorySuccess(t)
 
 	requestBody := strings.NewReader(``)
-	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/categories/100", requestBody)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories/100", requestBody)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -187,7 +208,7 @@ func TestUpdateCategorySuccess(t *testing.T) {
 	TestCreateCategorySuccess(t)
 
 	requestBody := strings.NewReader(`{"name":"Old Gadget"}`)
-	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/categories/1", requestBody)
+	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/api/categories/1", requestBody)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -213,7 +234,7 @@ func TestUpdateCategoryFailed(t *testing.T) {
 	TestCreateCategorySuccess(t)
 
 	requestBody := strings.NewReader(`{"name":""}`)
-	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/categories/1", requestBody)
+	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/api/categories/1", requestBody)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -237,7 +258,7 @@ func TestDeleteCategorySuccess(t *testing.T) {
 	router := setupRouter(db)
 	TestCreateCategorySuccess(t)
 
-	request := httptest.NewRequest(http.MethodDelete, "http://localhost:3000/categories/1", nil)
+	request := httptest.NewRequest(http.MethodDelete, "http://localhost:3000/api/categories/1", nil)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -261,7 +282,7 @@ func TestDeleteCategoryFailed(t *testing.T) {
 	router := setupRouter(db)
 	TestCreateCategorySuccess(t)
 
-	request := httptest.NewRequest(http.MethodDelete, "http://localhost:3000/categories/100", nil)
+	request := httptest.NewRequest(http.MethodDelete, "http://localhost:3000/api/categories/100", nil)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-API-Key", "RAHASIA")
 
@@ -287,7 +308,7 @@ func TestAuthorized(t *testing.T) {
 	router := setupRouter(db)
 	TestCreateCategorySuccess(t)
 
-	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/categories", nil)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories", nil)
 	request.Header.Add("Content-Type", "application/json")
 
 	recorder := httptest.NewRecorder()
