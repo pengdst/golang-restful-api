850457ae791caf38e5185bfebe11edb61f034368
pengdst <pengkuh.dst@gmail.com>
2022-07-04 12:44:43 +0700

    renew auth middleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,31 +1,13 @@
 package middleware
 
 import (
-	"net/http"
-	"pengdst/golang-restful-api/helper"
-	"pengdst/golang-restful-api/model/web"
+	"github.com/gin-gonic/gin"
+	"pengdst/golang-restful-api/exception"
 )
 
-type AuthMiddleware struct {
-	Handler http.Handler
-}
-
-func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
-	return &AuthMiddleware{Handler: handler}
-}
-
-func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	if "RAHASIA" == request.Header.Get("X-API-Key") {
-		middleware.Handler.ServeHTTP(writer, request)
-	} else {
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(http.StatusUnauthorized)
-
-		webResponse := web.WebResponse{
-			Code:   http.StatusUnauthorized,
-			Status: "Unauthorized",
-		}
-
-		helper.WriteToResponseBody(writer, webResponse)
+func AuthMiddleware(c *gin.Context) {
+	auth := c.GetHeader("X-API-Key")
+	if auth != "RAHASIA" {
+		panic(exception.NewValidationError("wrong api key"))
 	}
 }
