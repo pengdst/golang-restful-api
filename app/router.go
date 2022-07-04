7bf92223a446d28f83b9080fd3e402f4327b9e69
pengdst <pengkuh.dst@gmail.com>
2022-07-04 12:45:11 +0700

    create api group with middleware

--- app/router.go
+++ app/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"pengdst/golang-restful-api/controller"
 	"pengdst/golang-restful-api/exception"
+	"pengdst/golang-restful-api/middleware"
 )
 
 func NewRouter(categoryController controller.CategoryController) *gin.Engine {
@@ -12,11 +13,13 @@ func NewRouter(categoryController controller.CategoryController) *gin.Engine {
 	router.Use(gin.CustomRecovery(exception.ErrorHandler))
 
 	router.Static("/docs", "./docs")
-	router.GET("/categories", categoryController.GetAll)
-	router.POST("/categories", categoryController.Create)
-	router.GET("/categories/:categoryId", categoryController.FindById)
-	router.PUT("/categories/:categoryId", categoryController.Update)
-	router.DELETE("/categories/:categoryId", categoryController.Delete)
+
+	api := router.Group("api").Use(middleware.AuthMiddleware)
+	api.GET("/categories", categoryController.GetAll)
+	api.POST("/categories", categoryController.Create)
+	api.GET("/categories/:categoryId", categoryController.FindById)
+	api.PUT("/categories/:categoryId", categoryController.Update)
+	api.DELETE("/categories/:categoryId", categoryController.Delete)
 
 	return router
 }
